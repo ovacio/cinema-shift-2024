22fd9dd984cc0892e586410e8421a9497e930fc2
Daniil <kokolovdaniil@gmail.com>
2024-01-30 14:32:38 +0300

    Create js.js

---
+++ js/js.js
@@ -0,0 +1,11 @@
+const API_KEY = "64c1ef30-b947-47e1-a587-82876dcc3e93";
+const API_Films_Today = "https://shift-backend.onrender.com/cinema/today"
+
+async function getMovies(url)
+{
+    const response = await fetch(url, {
+        headers: {
+            "Content-Type": ""
+        }
+    })
+}
